30892c9588ee2d6b117c0e6fb1594343d07ab897
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-17 20:54:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,9 +15,7 @@ class Song
   
   def artist=(artist)
     @artist=artist
-    if @artist.songs.include?(self) ==false 
-      @artist.songs << self
-    end
+    @artist.add_song(self)
   end
   def genre=(genre)
     @genre = genre
